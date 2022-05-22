b8638d91aa22e86e4194267a8b7b247326a02b9c
sabbirmahmud4455 <sabbirmahmud4455@gmail.com>
2022-05-23 00:04:40 +0600

    condition, list and key

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Button from "./ButtonComponent"
 
 class TestComponent extends React.Component{
 
-	state = {
-		date: new Date() ,
-		locale: 'bn-BD'
-	};
+	
+
+	constructor(props){
+		super(props)
+		this.state = {
+			date: new Date() ,
+			locale: 'bn-BD'
+		};
+	}
 
 	componentDidMount = () => {
 		this.clockTimer = setInterval(() => {this.tick()}, 1000);
@@ -25,19 +29,22 @@ class TestComponent extends React.Component{
 
 	click = (locale) => {
 
-		this.setState({locale})
-
-		
+		this.setState({locale : this.state.locale === 'bn-BD' ? "en-US" : "bn-BD"})
 	}
 
 	render(){
 		const {date, locale} = this.state;
-
+		const arr = [1,2,3,4,5,6]
 		return(
 			<div>
 				<h1>hello {date.toLocaleTimeString(locale)}</h1>
 
-				<Button clickk={this.click} locale="en-US"></Button>
+				<button type="button" locale="locale" onClick={this.click} > {locale === "bn-BD" ? 'en' : 'bn'} </button>
+
+				{arr.map((val, index) => {
+					return val === 2 ? (<p key={val}>hello {val}</p>) : '';
+				})}
+
 			</div>
 			
 		)
